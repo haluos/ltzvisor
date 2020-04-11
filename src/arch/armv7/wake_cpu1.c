0b168a31e0a67a3dd97ae29c746085ef41db3f24
haluos <mhalusek@gmail.com>
2020-04-11 21:37:05 +0200

    fixed wake up CPU1 signal

--- src/arch/armv7/wake_cpu1.c
+++ src/arch/armv7/wake_cpu1.c
@@ -1,9 +1,11 @@
 #include "io.h"
+#include <printk.h>
 
 #define APP_CPU1_ADDR	0x1C010000
 #define CPU1_CATCH		0xFFFFFFF0
 
 void add_entry_addr_cpu1()
 {
-  write32(CPU1_CATCH,APP_CPU1_ADDR);
+  printk("Wake CPU1\r\n");
+  write32((volatile uint32_t *)CPU1_CATCH,APP_CPU1_ADDR);
 }
--- src/core/ltzvisor.c
+++ src/core/ltzvisor.c
@@ -11,28 +11,28 @@
  *
  * LTZVisor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation, with a special   
+ * as published by the Free Software Foundation, with a special
  * exception described below.
- * 
- * Linking this code statically or dynamically with other modules 
- * is making a combined work based on this code. Thus, the terms 
- * and conditions of the GNU General Public License V2 cover the 
+ *
+ * Linking this code statically or dynamically with other modules
+ * is making a combined work based on this code. Thus, the terms
+ * and conditions of the GNU General Public License V2 cover the
  * whole combination.
  *
- * As a special exception, the copyright holders of LTZVisor give  
- * you permission to link LTZVisor with independent modules to  
- * produce a statically linked executable, regardless of the license 
- * terms of these independent modules, and to copy and distribute  
- * the resulting executable under terms of your choice, provided that 
- * you also meet, for each linked independent module, the terms and 
- * conditions of the license of that module. An independent module  
+ * As a special exception, the copyright holders of LTZVisor give
+ * you permission to link LTZVisor with independent modules to
+ * produce a statically linked executable, regardless of the license
+ * terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that
+ * you also meet, for each linked independent module, the terms and
+ * conditions of the license of that module. An independent module
  * is a module which is not derived from or based on LTZVisor.
  *
  * LTZVisor is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -41,12 +41,13 @@
  * [ltzvisor.c]
  *
  * This file contains the ltzvisor main entry point.
- * 
+ *
  * (#) $id: ltzvisor.c 04-05-2015 s_pinto & j_pereira $
  * (#) $id: ltzvisor.c 16-09-2017 s_pinto (modified)$
 */
 
 #include <ltzvisor.h>
+#include <ltzvisor_hw.h>
 
 /** Config NS_Guest struct */
 extern struct nsguest_conf_entry nsguest_config;
@@ -57,9 +58,9 @@ tzmachine NS_Guest __attribute__ ((aligned (4))) __attribute__ ((section (".bss"
 /**
  * LTZVisor main entry point
  *
- * @param  	
+ * @param
  *
- * @retval 	
+ * @retval
  */
 void ltzvisor_main(void){
 	uint32_t ret;
@@ -91,3 +92,9 @@ void ltzvisor_main(void){
 	while (1);
 }
 
+void LTZVisor_CPU1_entry (void)
+{
+	printk("LTZVisor running on CPU1\r\n");
+	interrupt_interface_init();
+	LTZVISOR_MON_EXIT()
+}
